a95c296f06cd34d513bc4f945fdbaef7b7a44f71
Drelf2018 <drelf2018@outlook.com>
2023-08-05 14:08:37 +0800

    fix: 终于实现了

--- code/main.go
+++ code/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"os"
 	"os/exec"
 
 	"github.com/Drelf2018/webhook"
 )
 
 func main() {
-	exec.Command("git", "clone", "-b", "gh-pages", "https://github.com/Drelf2018/nana7mi.link.git").Run()
+	_, err := os.Stat("nana7mi.link")
+	if err != nil {
+		exec.Command("git", "clone", "-b", "gh-pages", "https://github.com/Drelf2018/nana7mi.link.git").Run()
+	}
 	webhook.Run(&webhook.Config{})
 }
