f48e0096faf54c82fbc417602091c94a8ebcb55c
Drelf2018 <drelf2018@outlook.com>
2023-08-05 14:02:51 +0800

    Update main.go

--- code/main.go
+++ code/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"os/exec"
+
 	"github.com/Drelf2018/webhook"
 )
 
 func main() {
+	exec.Command("git", "clone", "-b", "gh-pages", "https://github.com/Drelf2018/nana7mi.link.git").Run()
 	webhook.Run(&webhook.Config{})
 }
