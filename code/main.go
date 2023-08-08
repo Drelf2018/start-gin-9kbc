9113e776271f3d63462bf874805bb8ec479e4520
Drelf2018 <drelf2018@outlook.com>
2023-08-08 16:59:15 +0800

    update

--- code/main.go
+++ code/main.go
@@ -5,6 +5,5 @@ import "github.com/Drelf2018/webhook"
 func main() {
 	webhook.Run(&webhook.Config{
 		Resource: webhook.Resource{Path: "/home/app"},
-		Github:   webhook.Github{Repository: "gin.nana7mi.link"},
 	})
 }
